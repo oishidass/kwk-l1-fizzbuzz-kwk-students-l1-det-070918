2f0938af06d82232418b2d44ea0e0a116f76fadc
oishidass <oishidass1234@gmail.com>
2018-07-17 14:35:10 +0000

    automatically backed up by learn

---
+++ practicestyle.rb
@@ -0,0 +1,19 @@
+.navigation {
+  
+  width: 100%;
+  height: 50px;
+  background-color: lightblue; 
+}
+  
+  hi {
+    
+    text-align: cente;
+    font-size: 400%;
+  }
+    
+    body {
+      
+      background: 
+      url(https://www.uncf.org/i/local_offices/VJP_4376_Detroit_Nighttime_Skyline_Vito_Palmisano_high.jpg);
+      background-size:
+    }
